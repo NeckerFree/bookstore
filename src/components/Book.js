bf6f4d46f9a8214895208a8d23f0fb87e0869e9c
Elio Cortes <elio.cortes3000@gmail.com>
2022-05-13 12:26:38 -0500

    Implemented Styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -22,12 +22,28 @@ const Book = (props) => {
         <p className="gender">{category}</p>
         <p className="title">{title}</p>
         <p className="author">{author}</p>
+        <ul className="listItems">
+          <li><a href="#top" onClick={foo}>Comments</a></li>
+          <li><a href="#top" id={id} onClick={handleRemoveBook}>Remove</a></li>
+          <li><a href="#top" onClick={foo}>Edit</a></li>
+        </ul>
       </div>
-      <ul className="listItems">
-        <li><a href="#top" onClick={foo}>Comments</a></li>
-        <li><a href="#top" id={id} onClick={handleRemoveBook}>Remove</a></li>
-        <li><a href="#top" onClick={foo}>Edit</a></li>
-      </ul>
+      <div className="completed">
+        <div className="progressContainer">
+          <div className="progress" />
+        </div>
+        <div>
+          <div>
+            <h1 className="value">75%</h1>
+            <p className="completedText">Completed</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="curent-chapter">Current chapter</p>
+        <p className="chapter">Chapter 10</p>
+        <input type="submit" value="Update Progress" />
+      </div>
     </section>
   );
 };
